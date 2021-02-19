d275100253e6a0abda5bd371745d698f0ac01744
Mikhail Bhuta <mikhail.b1990@gmail.com>
2021-02-20 03:30:57 +0530

    Initial commit

---
+++ Assets/ProceduralPlanetSeries/Scripts/Planet.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public bool autoUpdate = true;
+
+    [Range(2, 256)]
+    public int resolution = 10;
+
+    public ShapeSettings shapeSettings;
+    public ColorSettings colorSettings;
+
+    [HideInInspector]
+    public bool shapeSettingsFoldout;
+    [HideInInspector]
+    public bool colorSettingsFoldout;
+
+    [SerializeField, HideInInspector]
+    private MeshFilter[] meshFilters;
+    private TerrainFace[] terrainFaces;
+
+    private ShapeGenerator shapeGenerator;
+
+    private void Initialize()
+    {
+        shapeGenerator = new ShapeGenerator(shapeSettings);
+
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    public void GeneratePlanet()
+    {
+        Initialize();
+        GenerateMesh();
+        GenerateColors();
+    }
+
+    private void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+
+    private void GenerateColors()
+    {
+        foreach (MeshFilter m in meshFilters)
+        {
+            m.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.planetColor;
+        }
+    }
+
+    public void OnColorSettingsUpdated()
+    {
+        if (!autoUpdate) return;
+        Initialize();
+        GenerateColors();
+    }
+
+    public void OnShapeSettingsUpdated()
+    {
+        if (!autoUpdate) return;
+        Initialize();
+        GenerateMesh();
+    }
+}
---
+++ Assets/ProceduralPlanetSeries/Scripts/SettingsScripts/NoiseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    [Range(1, 8)]
+    public int numLayers = 1;
+    public float persistence = 0.5f;
+    public float strength = 1f;
+    public float baseRoughness = 1f;
+    public float roughness = 2f;
+    public Vector3 center;
+    public float minValue;
+}
