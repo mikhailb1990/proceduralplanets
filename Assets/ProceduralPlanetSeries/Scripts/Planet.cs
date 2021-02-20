7fab752d18ba564e89a75cd24daed2a7f774cf5b
Mikhail Bhuta <mikhail.b1990@gmail.com>
2021-02-20 14:00:32 +0530

    Episode 4 Complete: Multiple Noise Filters implemented - Simple and Ridgid

--- Assets/ProceduralPlanetSeries/Scripts/Planet.cs
+++ Assets/ProceduralPlanetSeries/Scripts/Planet.cs
@@ -9,6 +9,9 @@ public class Planet : MonoBehaviour
     [Range(2, 256)]
     public int resolution = 10;
 
+    public enum FaceRenderMask { All, Top, Bottom, Left, Right, Forward, Back }
+    public FaceRenderMask faceRenderMask;
+
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
 
@@ -49,6 +52,8 @@ public class Planet : MonoBehaviour
             }
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -61,9 +66,12 @@ public class Planet : MonoBehaviour
 
     private void GenerateMesh()
     {
-        foreach (TerrainFace face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
